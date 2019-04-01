ef960015abda88359391df51efe72ed52739f967
Agustin <agustin@Macbook-Pro-MF.local>
2019-04-01 13:17:08 -0300

    ~

--- game.js
+++ game.js
@@ -1,29 +1,102 @@
-var canvas = null,
-    ctx = null;
-var x = 50;
-    y = 50;
+var KEY_ENTER = 13,
+    KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40,
+    
+    canvas = null,
+    ctx = null,
+    lastPress = null,
+    pause = true,
+    x = 50,
+    y = 50,
+    dir = 0;
 
+
+document.addEventListener('keydown', evt => {
+    lastPress = evt.which;
+}, false);
+
+
+function act(){
+    if (!pause) {
+        // Change Direction
+        if (lastPress == KEY_UP) {
+            dir = 0;
+        }
+        if (lastPress == KEY_RIGHT) {
+            dir = 1;
+        }
+        if (lastPress == KEY_DOWN) {
+            dir = 2;
+        }
+        if (lastPress == KEY_LEFT) {
+            dir = 3;
+        }
+
+        // Move Rect
+        if (dir == 0) {
+            y -= 10;
+        }
+        if (dir == 1) {
+            x += 10;
+        }
+        if (dir == 2) {
+            y += 10;
+        }
+        if (dir == 3) {
+            x -= 10;
+        }
+
+        // Out Screen
+        if (x > canvas.width) {
+            x = 0;
+        }
+        if (y > canvas.height) {
+            y = 0;
+        }
+        if (x < 0) {
+            x = canvas.width;
+        }
+        if (y < 0) {
+            y = canvas.height;
+        }
+    }
+    
+    // Pause/Unpause
+    if (lastPress == KEY_ENTER) {
+        pause = !pause;
+        lastPress = null;
+    }
+}
 function paint(ctx) {
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x, y, 10, 10);
-}
-function act(){
-    x += 2;
-    if (x > canvas.width) {
-        x = 0;
+
+    if (pause) {
+        ctx.textAlign = 'center';
+        ctx.fillText('PAUSE', canvas.width / 2, canvas.height / 2  );
+        ctx.textAlign = 'left';
     }
 }
-function run() {
-    window.requestAnimationFrame(run);
-    act();
+
+function repaint() {
+    window.requestAnimationFrame(repaint);
     paint(ctx);
 }
+
+function run() {
+    setTimeout(run, 50);
+    act();
+}
+
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     run();
+    repaint();
 }
 window.addEventListener('load', init, false);
